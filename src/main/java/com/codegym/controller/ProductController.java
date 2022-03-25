1e9eb336b33b67ee37ad2a4b93917f442b13dab0
daothannhangian <binhk4a@ gmail.com>
2022-03-25 14:37:08 +0700

    bt

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -1,9 +1,10 @@
 package com.codegym.controller;
 
-
+import com.codegym.model.Category;
 import com.codegym.model.Product;
 import com.codegym.model.ProductForm;
-import com.codegym.service.IProductService;
+import com.codegym.service.category.ICategoryService;
+import com.codegym.service.product.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -14,7 +15,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -22,14 +22,17 @@ public class ProductController {
     @Autowired
     private IProductService productService;
 
+    @Autowired
+    private ICategoryService categoryService;
+
     @Value("${file-upload}")
     private String uploadPath;
 
     @GetMapping("/products/list")
-    public ModelAndView showListProduct(@RequestParam(name = "q")Optional<String> q) {
+    public ModelAndView showListProduct(@RequestParam(name = "q") Optional<String> q) {
         ModelAndView modelAndView = new ModelAndView("/product/list");
-        List<Product> products = productService.findAll();
-        if (q.isPresent()){
+        Iterable<Product> products = productService.findAll();
+        if (q.isPresent()) {
             products = productService.findProductByNameContaining(q.get());
         }
         modelAndView.addObject("products", products);
@@ -38,26 +41,34 @@ public class ProductController {
 
     @GetMapping("/products/delete/{id}")
     public ModelAndView showDeleteProduct(@PathVariable Long id) {
+        Optional<Product> productOptional = productService.findById(id);
+        if (!productOptional.isPresent()) {
+            return new ModelAndView("error-404");
+        }
         ModelAndView modelAndView = new ModelAndView("/product/delete");
-        Product product = productService.findById(id);
-        modelAndView.addObject("product", product);
+        modelAndView.addObject("product", productOptional.get());
         return modelAndView;
     }
 
     @PostMapping("/products/delete/{id}")
     public ModelAndView deleteProduct(@PathVariable Long id) {
-        Product product = productService.findById(id);
-        File file = new File(uploadPath + product.getImage());
-        if (file.exists()){
-            file.delete();
+        Optional<Product> product = productService.findById(id);
+        if (product.isPresent()) {
+            File file = new File(uploadPath + product.get().getImage());
+            if (file.exists()) {
+                file.delete();
+            }
+            productService.removeById(id);
+            return new ModelAndView("redirect:/products/list");
         }
-        productService.removeById(id);
-        return new ModelAndView("redirect:/products/list");
+        return new ModelAndView("error-404");
     }
 
     @GetMapping("/products/create")
     public ModelAndView showCreateProduct() {
         ModelAndView modelAndView = new ModelAndView("/product/create");
+        Iterable<Category> categories = categoryService.findAll();
+        modelAndView.addObject("categories", categories);
         modelAndView.addObject("product", new ProductForm());//Gửi 1 đối tượng product rỗng sang file view để tạo mới
         return modelAndView;
     }
@@ -73,47 +84,64 @@ public class ProductController {
             e.printStackTrace();
         }
         Product product = new Product(productForm.getId(), productForm.getName(), productForm.getPrice(), productForm.getDescription(), fileName);
+        product.setCategory(productForm.getCategory());
         productService.save(product);
         return new ModelAndView("redirect:/products/list");
     }
 
     @GetMapping("/products/edit/{id}")
     public ModelAndView showEditForm(@PathVariable Long id) {
+        Optional<Product> productOptional = productService.findById(id);
+        if (!productOptional.isPresent()) {
+            return new ModelAndView("error-404");
+        }
         ModelAndView modelAndView = new ModelAndView("/product/edit");
-        Product product = productService.findById(id);
-        modelAndView.addObject("product", product);
+        modelAndView.addObject("product", productOptional.get());
         return modelAndView;
     }
 
     @PostMapping("/products/edit/{id}")
     public ModelAndView editProduct(@PathVariable Long id, @ModelAttribute ProductForm productForm) {
         MultipartFile img = productForm.getImage();
-        Product oldProduct = productService.findById(id);
-        File file = new File(uploadPath + oldProduct.getImage());
-        if (file.exists()){
-            file.delete();
-        }
-        if (img.getSize() != 0) {
-            String fileName = productForm.getImage().getOriginalFilename();
-            long currentTime = System.currentTimeMillis(); //Xử lý lấy thời gian hiện tại
-            fileName = currentTime + fileName;
-            oldProduct.setImage(fileName);
-            try {
-                FileCopyUtils.copy(img.getBytes(), new File(uploadPath + fileName));
-            } catch (IOException e) {
-                e.printStackTrace();
+        Optional<Product> product = productService.findById(id);
+        if (product.isPresent()) {
+            Product oldProduct = product.get();
+            if (img.getSize() != 0) {
+                String fileName = productForm.getImage().getOriginalFilename();
+                long currentTime = System.currentTimeMillis(); //Xử lý lấy thời gian hiện tại
+                fileName = currentTime + fileName;
+                oldProduct.setImage(fileName);
+                try {
+                    FileCopyUtils.copy(img.getBytes(), new File(uploadPath + fileName));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
+            oldProduct.setPrice(productForm.getPrice());
+            oldProduct.setDescription(productForm.getDescription());
+            oldProduct.setName(productForm.getName());
+            productService.save(oldProduct);
+            return new ModelAndView("redirect:/products/list");
         }
-        oldProduct.setPrice(productForm.getPrice());
-        oldProduct.setDescription(productForm.getDescription());
-        oldProduct.setName(productForm.getName());
-        productService.save(oldProduct);
-        return new ModelAndView("redirect:/products/list");
+        return new ModelAndView("error-404");
     }
 
     @GetMapping("/products/{id}")
     public ModelAndView showProductDetail(@PathVariable Long id) {
-        Product product = productService.findById(id);
-        return new ModelAndView("/product/view", "product", product);
+        Optional<Product> productOptional = productService.findById(id);
+        if (!productOptional.isPresent()) {
+            return new ModelAndView("error-404");
+        }
+        ModelAndView modelAndView = new ModelAndView("/product/view");
+        modelAndView.addObject("product", productOptional.get());
+        return modelAndView;
+    }
+
+    @GetMapping("/products/search")
+    public ModelAndView showProductSearch(@RequestParam("min") Double min, @RequestParam("max") Double max) {
+        Iterable<Product> products = productService.findProductPriceBetween(min, max);
+        ModelAndView modelAndView = new ModelAndView("/product/list");
+        modelAndView.addObject("products", products);
+        return modelAndView;
     }
 }
