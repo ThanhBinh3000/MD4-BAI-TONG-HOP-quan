8bfa09f92baaec79c38122abc46d88bd215749fe
daothannhangian <binhk4a@ gmail.com>
2022-04-07 11:40:24 +0700

    jwt

--- src/main/java/com/codegym/config/security/SecurityInit.java
+++ src/main/java/com/codegym/config/security/SecurityInit.java
@@ -1,7 +1,6 @@
 package com.codegym.config.security;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
 }
